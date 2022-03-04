48a35b85edcba3aa98c12ebae83d31e7be29106d
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-04 10:20:04 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToUInt16(Console.ReadLine());
+            int m = Convert.ToUInt16(Console.ReadLine());
+            int s = 0;
+           while(n<m)
+            {
+                n++;
+                if (n % 7 == 0)
+                {
+                    s++;
+                }
+               
+               
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
